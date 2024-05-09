26842132a4edccfa63870de5d089ec47bfaee9b5
Kari Taylor <168463032+klg56880@users.noreply.github.com>
2024-05-08 21:55:12 -0500

    Create karitaylor_projsetup.py

---
+++ karitaylor_projsetup.py
@@ -0,0 +1,18 @@
+""" Start a Data Analytics Project"""
+
+import pathlib
+
+def create_project_directory(directory_name: str): # add type hinting to params
+    """
+    Creates a project sub-directory.
+    : param directory_name: Name to be created, e.g. "test"
+    """
+    pathlib.Path(directory_name).mkdir(exist_ok=True)
+
+def main():
+    """ Scaffold a project. """
+    create_project_directory(directory_name='test') # name the parameter
+    create_project_directory(directory_name='docs') # name the parameter
+
+if __name__ == '__main__':
+    main()
